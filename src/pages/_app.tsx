db68c5b016957dda12e5f301640f34c70f6027f6
David Sanchez <d4vsanchez@gmail.com>
2021-11-06 22:32:00 -0500

    Wrap App in registrar provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ComponentRegistrarProvider } from '@/hooks/useCustomComponent'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ComponentRegistrarProvider>
+      <Component {...pageProps} />
+    </ComponentRegistrarProvider>
+  )
 }
 
 export default MyApp
