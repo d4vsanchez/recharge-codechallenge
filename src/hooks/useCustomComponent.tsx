55887d5b3dd774463d02087f8ae0356694f5337c
David Sanchez <d4vsanchez@gmail.com>
2021-11-06 22:05:22 -0500

    Create the custom component hook

---
+++ src/hooks/useCustomComponent.tsx
@@ -0,0 +1,35 @@
+import { RegisterComponentListener } from '@/lib/CustomComponentRegistrar'
+import type { FC } from 'react'
+import * as React from 'react'
+import { useMemo, useCallback, useState, useEffect, useContext, createContext } from 'react'
+
+const ComponentRegistrarContext = createContext<(aSection: string) => (() => JSX.Element | null) | null>(() => null)
+
+export const ComponentRegistrarProvider: FC = ({ children }) => {
+  const { current: customComponentListener } = React.useRef(new RegisterComponentListener())
+  const [components, setComponent] = useState<Record<string, () => JSX.Element | null>>({})
+
+  useEffect(() => {
+    customComponentListener.addEventListener((theSection, theComponent) =>
+      setComponent((components) => ({ ...components, [theSection]: theComponent }))
+    )
+
+    return () => {
+      customComponentListener.removeEventListener()
+    }
+  }, [setComponent, customComponentListener])
+
+  const getComponentFor = useCallback((aSection: string) => components[aSection] || null, [components])
+
+  return <ComponentRegistrarContext.Provider value={getComponentFor}>{children}</ComponentRegistrarContext.Provider>
+}
+
+export function useComponentRegistrar() {
+  const context = useContext(ComponentRegistrarContext)
+
+  if (context === undefined) {
+    throw new Error('error')
+  }
+
+  return context
+}
