8d2810bbb2c39551d062dcd7a2eb49d8dfd77a58
David Sanchez <d4vsanchez@gmail.com>
2021-11-06 23:00:51 -0500

    Create ProductList component

---
+++ src/components/product/list/list.tsx
@@ -0,0 +1,19 @@
+import type { FC } from 'react'
+import type { Product as ProductType } from '@/types/Product'
+
+import styles from '@/styles/ProductList.module.css'
+import { ProductContainer } from '@/components/product/product-container'
+
+interface ProductListProps {
+  products: Array<ProductType>
+}
+
+export const ProductList: FC<ProductListProps> = ({ products }) => {
+  return (
+    <div className={styles.grid}>
+      {products.map((product) => (
+        <ProductContainer key={product.id} product={product} />
+      ))}
+    </div>
+  )
+}
